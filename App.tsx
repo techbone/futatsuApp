74921f2b9c486cf1b28a0515511787805eea5fd6
Etudaye <musaawwaletudaye@gmail.com>
2024-07-26 23:55:54 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx ti initiateText>
+      <Text>Open up App.tsx to initiateText>
       <StatusBar style="auto" />
     </View>
   );
